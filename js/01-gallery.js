254f6949f8eb589d38f1f96b6facdbbb74012185
Tatiana Kashcha <tkashcha@gmail.com>
2023-03-29 19:14:50 +0300

    додає 1 частину 01дз

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,6 +1,6 @@
 import { galleryItems } from "./gallery-items.js";
-// Change code below this line
 
+// Шаблон розмітки галереї
 {
   /* <li class="gallery__item">
   <a class="gallery__link" href="large-image.jpg">
@@ -12,6 +12,41 @@ import { galleryItems } from "./gallery-items.js";
     />
   </a>
 </li>; */
+}
+
+const galleryItemsEl = document.querySelector(".gallery");
+const makeGalleryItems = (itemsEl) => {
+  const { preview, original, description } = itemsEl;
+  return `<li class="gallery__item">
+  <a class="gallery__link" href=${original}>
+    <img
+      class="gallery__image"
+      src=${preview}
+      data-source=${original}
+      alt=${description}
+    />
+  </a>
+</li>`;
+};
+const itemsImages = galleryItems.map(makeGalleryItems).join("");
+galleryItemsEl.insertAdjacentHTML("beforeend", itemsImages);
+
+galleryItemsEl.addEventListener("click", onImgGalleryClick);
+
+function onImgGalleryClick(evt) {
+  evt.preventDefault();
+  const ImgGalleryEl = evt.target.classList.contains("gallery__image");
+
+  if (!ImgGalleryEl) {
+    return;
+  }
+
+  const imgElsource = evt.target.dataset.source;
+  const instance = basicLightbox.create(`
+      <img src='${imgElsource}'>
+  `);
+
+  instance.show();
 }
 
 console.log(galleryItems);
